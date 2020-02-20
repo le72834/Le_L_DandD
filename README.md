b302684f63866e5fe0285e17f5e71d33bb475cb5
Wendy Le <lehong@Les-MacBook-Pro.local>
2020-02-19 19:44:34 -0500

    adding solution

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# This is a project about DandD
+![All About the Drag and Drop Puzzle Game][images/hero_image.png]
+
+# A Drag and Drop Puzzle Game.
+This is a simple drag and drop game with 4 different broad game. Each broad game have different picture we have to solve. We just need drag and drop to make the image complete.
+
+### Getting Started
+This is a basic HTML/ CSS/ JavaScript build. If you don't know what any of these are, start here: www.mozilla.com
+
+Clone this to the location of your choice, open in your favourite editor and hack away!
+
+### Prerequisites
+Just an editor and Git!
+
+### Author
+1. Linh Le
+
+### License
+Fanshawe College - All rights reserved.
--- script/app.js
+++ script/app.js
@@ -5,14 +5,16 @@
 				puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
 				dropZones = document.querySelectorAll('.drop-zone'),
 				gameBoard = document.querySelector('.puzzle-board');
+
 	const pieceName =["topLeft", "topRight", "bottomLeft", "bottomRight"];
 	function changeImageSet () {
 		//change all the image elements on the page -> draggable image sources,
 
-		//change the image elements on the left to match the seletecd puzzle
+		//change the image elements on the left to match the selected puzzle
 		pieceName.forEach((piece, index) => {
 		puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg` ;
 		puzzlePieces[index].id =`${piece + this.dataset.puzzleref}`;
+
 
 	});
 
@@ -21,31 +23,44 @@
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
 		//debugger;
 	}
+
 	function allowDrag(event) {
-		console.log('started draggin an image');
+
+		console.log('started dragging an image');
 
 		event.dataTransfer.setData("text/plain", this.id);
+
 	}
 	function allowDragOver(event) {
+		if (this.childNodes.length == 0){
 		event.preventDefault();
 		console.log('dragged over a drop zone');
+		}
 	}
 	function allowDrop(event) {
+
 		//event.prevenDefault();
+
+		if (event.preventDefault) event.preventDefault();
+
+		if (event.stopPropagation) event.stopPropagation();
+
 		console.log('dropped on a drop zone');
 		//go and get the dragged element's ID from the data transfer
 		let currentImage = event.dataTransfer.getData("text/plain");
-
 		//add that image to whatever drop zone we're dropping out image on
 		event.target.appendChild(document.querySelector(`#${currentImage}`));
+	
+
 	}
 	//add event handling here
 
-
-
-
 	//click on the bottom buttons to change the puzzle image we're woking with
-	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+	puzzleButtons.forEach(button => {
+		button.addEventListener('click', changeImageSet);
+
+
+	});
 
 	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 
@@ -53,6 +68,7 @@
 	dropZones.forEach(zone => {
 		zone.addEventListener('dragover', allowDragOver);
 		zone.addEventListener('drop', allowDrop);
+
 	});
 
 	//call the function and pass in the first nav button as a reference
